e1ec00351791c60b7825dbfb093e5631bf5a9d1f
Michael Fronistas <m.k.fronistas2@newcastle.ac.uk>
2021-11-04 00:07:32 +0000

    Fixed bug where the running the lottery where the decrypted results would be added to the database. Used decrypt() method instead of .view_draw() because it gave a memory error. Also added comments to the file

--- admin/views.py
+++ admin/views.py
@@ -3,7 +3,7 @@ import copy
 
 from flask import Blueprint, render_template, request, flash
 from app import db, requires_roles
-from models import User, Draw
+from models import User, Draw, decrypt
 from flask_login import login_required, current_user
 
 # CONFIG
@@ -33,7 +33,6 @@ def view_all_users():
 @login_required
 @requires_roles('admin')
 def create_winning_draw():
-
     # get current winning draw
     current_winning_draw = Draw.query.filter_by(win=True).first()
     round = 1
@@ -54,8 +53,9 @@ def create_winning_draw():
     # remove any surrounding whitespace
     submitted_draw.strip()
 
-    # create a new draw object with the form data.
-    new_winning_draw = Draw(user_id=current_user.id, draw=submitted_draw, win=True, round=round,draw_key=current_user.draw_key)
+    # create a new draw object with the form data and the user id and draw key of the current user.
+    new_winning_draw = Draw(user_id=current_user.id, draw=submitted_draw, win=True, round=round,
+                            draw_key=current_user.draw_key)
 
     # add the new winning draw to the database
     db.session.add(new_winning_draw)
@@ -71,19 +71,16 @@ def create_winning_draw():
 @login_required
 @requires_roles('admin')
 def view_winning_draw():
-
-    # get winning draw from DB
-    # TODO not sure how it works but it does
+    # get winning draw from DB and decrypting it
     current_winning_draw = Draw.query.filter_by(win=True).first()
     decrypted_winning_draw = current_winning_draw.view_draw(current_user.draw_key)
-
 
     # if a winning draw exists
     if current_winning_draw:
         # re-render admin page with current winning draw and lottery round
         return render_template('admin.html', winning_draw=current_winning_draw, name=current_user.firstname)
 
-    # if no winning draw exists, rerender admin page
+    # if no winning draw exists, rerender admin page and return error message
     flash("No winning draw exists. Please add winning draw.")
     return admin()
 
@@ -93,16 +90,22 @@ def view_winning_draw():
 @login_required
 @requires_roles('admin')
 def run_lottery():
-
-    # get current unplayed winning draw
+    # get current unplayed winning draw and make a copy for decryption
     current_winning_draw = Draw.query.filter_by(win=True, played=False).first()
+    current_winning_draw_copy = copy.deepcopy(current_winning_draw)
 
     # if current unplayed winning draw exists
     if current_winning_draw:
 
         # get all unplayed user draws
         user_draws = Draw.query.filter_by(win=False, played=False).all()
+        # copy the list for decryption which wont affect database
+        draw_copies = list(map(lambda x: copy.deepcopy(x), user_draws))
         results = []
+        # decrypt all draws in the draw_copy list
+        for f in draw_copies:
+            user = User.query.filter_by(id=f.user_id).first()
+            decrypt(f.draw, user.draw_key)
 
         # if at least one unplayed user draw exists
         if user_draws:
@@ -118,11 +121,11 @@ def run_lottery():
                 # get the owning user (instance/object)
                 user = User.query.filter_by(id=draw.user_id).first()
 
-                # if user draw matches current unplayed winning draw
-                if draw.draw == current_winning_draw.draw:
-
+                # if user draw matches current unplayed winning draw, check with the decrypted copies of the draws
+                if decrypt(draw.draw, user.draw_key) == decrypt(current_winning_draw_copy.draw, current_user.draw_key):
                     # add details of winner to list of results
-                    results.append((current_winning_draw.round, draw.draw, draw.user_id, user.email))
+                    results.append((current_winning_draw_copy.round, decrypt(draw.draw, user.draw_key),
+                                    draw.user_id, user.email))
 
                     # update draw as a winning draw (this will be used to highlight winning draws in the user's
                     # lottery page)
@@ -133,7 +136,6 @@ def run_lottery():
 
                 # update draw with current lottery round
                 draw.round = current_winning_draw.round
-
                 # commit draw changes to DB
                 db.session.add(draw)
                 db.session.commit()
@@ -157,6 +159,7 @@ def run_lottery():
 @login_required
 @requires_roles('admin')
 def logs():
+    # opening the file and spliting the last 10 lines in reverse order so last shows first
     with open('lottery.log', 'r') as f:
         content = f.read().splitlines()[-10:]
         content.reverse()
