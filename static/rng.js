7bd58300abe1a3a4610699e38528e8e3aeb290d4
Michael Fronistas <m.k.fronistas2@newcastle.ac.uk>
2021-11-04 00:02:39 +0000

    Improved random numbers by implementing the crypto library

--- static/rng.js
+++ static/rng.js
@@ -6,9 +6,12 @@ function luckyDip() {
 
     // while set does not contain 6 values, create a random value between 1 and 60
     while (draw.size < 6) {
-        min = Math.ceil(1);
-        max = Math.floor(60);
-        value = Math.floor(Math.random() * (max - min + 1) + min);
+        let randomBuffer = new Uint32Array(1);
+        window.crypto.getRandomValues(randomBuffer);
+        let randomNumber = randomBuffer[0] / (0xFFFFFFFF);
+        let min = Math.ceil(1);
+        let max = Math.floor(60);
+        let value = Math.floor(randomNumber * (max - min + 1) + min);
 
         // sets cannot contain duplicates so value is only added if it does not exist in set
         draw.add(value)
